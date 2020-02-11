82d9bf38a1971120287bd83d458175e1afb9a8ce
kdilot <kdilotdev@gmail.com>
2020-02-11 17:07:02 +0900

    test

--- src/common/components/SampleList/index.tsx
+++ src/common/components/SampleList/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from './styles';
 
 const SampleList: React.FC = () => {
-    return <Container>asdf</Container>;
+    return <Container>test</Container>;
 };
 
 export default SampleList;
