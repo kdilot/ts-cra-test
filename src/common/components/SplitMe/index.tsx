43aa930b195c8b8cf4fdd4bc5da6cb7000fea023
kdilot <kdilotdev@gmail.com>
2020-02-12 10:26:47 +0900

    loadable component

---
+++ src/common/components/SplitMe/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SplitMe: React.FC = () => {
+    return <div>SplitMe</div>;
+};
+
+export default SplitMe;
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,2 +1,3 @@
 export { default as FlatList } from './components/FlatList';
 export { default as SampleList } from './components/SampleList';
+export { default as SplitMe } from './components/SplitMe';
