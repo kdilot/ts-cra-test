363eef2940fd84a434d7ff6d3e0f24e6948bbef2
kdilot <kdilotdev@gmail.com>
2020-03-02 13:39:41 +0900

    test

--- src/components/page/styles.tsx
+++ src/components/page/styles.tsx
@@ -2,8 +2,21 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     display: flex;
+    flex-direction: column;
     width: 100vw;
     height: 100vh;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
+    padding-top: 20%;
+`;
+
+export const Enter = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 2%;
+`;
+
+export const Title = styled.div`
+    font-size: 3rem;
+    font-weight: bold;
 `;
