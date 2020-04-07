a5e4a540149ca66784815d6cbe716a13ff8d1fb0
kdilot <kdilotdev@gmail.com>
2020-04-08 08:33:31 +0900

    search highlight

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import { Header, Content, Footer } from './styles';
-import { Link } from 'react-router-dom';
-import { Helmet } from 'react-helmet-async';
-import loadable from '@loadable/component';
+import React, { useState } from 'react';
+import { Content } from './styles';
+import styled from 'styled-components';
 
-const ImageUpload = loadable(() => import('common/components/ImageUpload'));
 // const Editor = loadable(() => import('common/components/Editor'));
+const ARRAY = [
+    'tliz',
+    'triz company',
+    'trizcompany',
+    'tliz com',
+    '트리즈',
+    '트리',
+    '투리즈투리즈',
+    '트리즈컴퍼니',
+    '트리즈 컴퍼니',
+];
 
 const Home: React.FC = () => {
+    const [list, setList] = useState<any>(ARRAY);
+    const [value, setValue] = useState('');
+
+    const onChange = (e: any) => {
+        if (e.target.value.trim()) {
+            setValue(e.target.value);
+            setList(
+                ARRAY.filter((text) => {
+                    return text.includes(e.target.value);
+                }),
+            );
+        } else {
+            setValue('');
+        }
+    };
     return (
         <>
-            <Helmet>
-                <title>메인 페이지</title>
-            </Helmet>
-            <Header>
-                <Link to="/page/home">{process.env.REACT_APP_GOOGLE_GA} </Link>
-            </Header>
-            <Header>
-                <Link to="/page/content">Content </Link>
-            </Header>
             <Content>
                 {/* <FlatList
                     data={DATA}
@@ -30,13 +44,39 @@ const Home: React.FC = () => {
                 {/* <Button text={'Download'} />
                 <Button text={'Test'} />
                 <Button text={'Button'} /> */}
-                <ImageUpload />
+                {/* <ImageUpload /> */}
+                <Input>
+                    <input onChange={(e) => onChange(e)}></input>
+                </Input>
+                <List>
+                    {list.map((m: any, i: number) => {
+                        const split = m.split(value);
+                        return value ? (
+                            <div key={i}>
+                                {split[0]}
+                                <Special>{value}</Special>
+                                {split[1]}
+                            </div>
+                        ) : (
+                            <div key={i}>{m}</div>
+                        );
+                    })}
+                </List>
             </Content>
-            <Footer>
-                <Link to="/page/footer">Footer </Link>
-            </Footer>
         </>
     );
 };
+
+const Input = styled.div`
+    height: 50px;
+`;
+
+const List = styled.div`
+    height: 400px;
+`;
+
+const Special = styled.span`
+    color: red;
+`;
 
 export default Home;
