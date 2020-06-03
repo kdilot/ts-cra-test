7bc164ea2365d103da0a7e6f3fc563d79f0b7698
kdilot <kdilotdev@gmail.com>
2020-06-03 13:51:04 +0900

    image test

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,130 +1,150 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Content } from './styles';
-import styled from 'styled-components';
-import { Test } from 'api/test';
-import { TwitterShareButton, FacebookShareButton } from 'react-share';
-import Clipboard from 'react-clipboard.js';
+import { img as testImg } from './test';
 
 // const Editor = loadable(() => import('common/components/Editor'));
-const ARRAY = [
-    'tliz',
-    'triz company',
-    'trizcompany',
-    'tliz com',
-    '트리즈',
-    '트리',
-    '투리즈투리즈',
-    '트리즈컴퍼니',
-    '트리즈 컴퍼니',
-];
-
 const Home: React.FC = () => {
-    const [list, setList] = useState<any>(ARRAY);
-    const [value, setValue] = useState('');
+    const [newfile, setFiles] = useState('');
+    const rotateImage: any = async (base64: any) => {
+        const filename = await new Promise(function (resolve, reject) {
+            let image = new Image();
+            image.src = base64;
+            image.onload = function () {
+                let resizedDataUrl = rotateResizeImage(image, 0.7);
+                resolve(resizedDataUrl);
+            };
+        });
+        return filename;
+    };
+    const resize: any = async (
+        base64: any,
+        maxWidth: number,
+        maxHeight: number,
+        orientation: number,
+    ) => {
+        const filename = await new Promise(function (resolve, reject) {
+            let image = new Image();
+            image.src = base64;
+            image.onload = function () {
+                let resizedDataUrl = resizeImage(
+                    image,
+                    maxWidth,
+                    maxHeight,
+                    0.7,
+                    orientation,
+                );
+                resolve(resizedDataUrl);
+            };
+            // let reader = new FileReader();
+            // reader.readAsDataURL(file);
+            // reader.onload = function (event: any) {
+            //     let dataUrl = event.target.result;
+            //     console.log(dataUrl);
+            //     let image = new Image();
+            //     image.src = dataUrl;
+            //     image.onload = function () {
+            //         let resizedDataUrl = resizeImage(
+            //             image,
+            //             maxWidth,
+            //             maxHeight,
+            //             0.7,
+            //         resolve(resizedDataUrl);
+            //     };
+            // };
+        });
+        return filename;
+    };
+    const resizeImage = (
+        image: any,
+        maxWidth: number,
+        maxHeight: number,
+        quality: number,
+        orientation: number,
+    ) => {
+        let canvas = document.createElement('canvas');
 
-    const onChange = (e: any) => {
-        if (e.target.value.trim()) {
-            setValue(e.target.value);
-            setList(
-                ARRAY.filter((text) => {
-                    return text.includes(e.target.value);
-                }),
-            );
+        let width = image.width;
+        let height = image.height;
+
+        if (width > height) {
+            if (width > maxWidth) {
+                height = Math.round((height * maxWidth) / width);
+                width = maxWidth;
+            }
         } else {
-            setValue('');
+            if (height > maxHeight) {
+                width = Math.round((width * maxHeight) / height);
+                height = maxHeight;
+            }
         }
+
+        // if (orientation >= 5 && orientation <= 8) {
+        //     canvas.width = 90 % 180 === 0 ? width : height;
+        //     canvas.height = 90 % 180 === 0 ? height : width;
+        // } else {
+        // }
+        canvas.width = width;
+        canvas.height = height;
+
+        let ctx: any = canvas.getContext('2d');
+
+        ctx.save();
+        // if (orientation >= 5 && orientation <= 8) {
+        //     ctx.translate(canvas.width / 2, canvas.height / 2);
+        //     ctx.rotate((90 * Math.PI) / 180);
+        //     ctx.drawImage(image, image.width / -2, image.height / -2);
+        // } else {
+        // }
+        ctx.drawImage(image, 0, 0, width, height);
+        ctx.restore();
+        return canvas.toDataURL('image/jpeg', quality);
+    };
+    const rotateResizeImage = (image: any, quality: number) => {
+        let canvas = document.createElement('canvas');
+
+        let width = image.width;
+        let height = image.height;
+
+        canvas.width = 90 % 180 === 0 ? width : height;
+        canvas.height = 90 % 180 === 0 ? height : width;
+
+        let ctx: any = canvas.getContext('2d');
+
+        ctx.save();
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        ctx.rotate((90 * Math.PI) / 180);
+        ctx.drawImage(image, image.width / -2, image.height / -2);
+        ctx.restore();
+        return canvas.toDataURL('image/jpeg', quality);
+    };
+
+    const start = async () => {
+        const test = await rotateImage(testImg);
+        console.log(test);
+        const file = await resize(test, 480, 320, -1);
+        setFiles(file);
     };
 
     useEffect(() => {
-        Test();
-    });
-
+        start();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return (
         <>
             <Content>
-                <Input>
-                    <input onChange={(e) => onChange(e)}></input>
-                </Input>
-                <List>
-                    {list.map((m: any, i: number) => {
-                        const split = m.split(value);
-                        return value ? (
-                            <div key={i}>
-                                {split[0]}
-                                <Special>{value}</Special>
-                                {split[1]}
-                            </div>
-                        ) : (
-                            <div key={i}>{m}</div>
-                        );
-                    })}
-                </List>
-                <Share>
-                    <TwitterShareButton url={'http://naver.com'}>
-                        <Box>T</Box>
-                    </TwitterShareButton>
-                    <FacebookShareButton url={'http://naver.com'}>
-                        <Box>F</Box>
-                    </FacebookShareButton>
-                    <Clipboard data-clipboard-text="http://naver.com">
-                        <Box>URL</Box>
-                    </Clipboard>
-                    <button
-                        onClick={() =>
-                            window.open(
-                                'https://story.kakao.com/share?url=https://naver.com',
-                                '_blank',
-                                `width=550 height=400 left=${
-                                    (window.screen.width - 550) / 2
-                                } top=${(window.screen.height - 400) / 2}`,
-                            )
-                        }>
-                        <Box>TEST</Box>
-                    </button>
-                </Share>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                    <img src={testImg} alt="asdf" />
+                    <img src={newfile} alt="asdf" />
+                </div>
             </Content>
         </>
     );
 };
-
-const Input = styled.div`
-    height: 50px;
-`;
-
-const List = styled.div`
-    height: 400px;
-`;
-
-const Special = styled.span`
-    color: red;
-`;
-
-const Share = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: row;
-    button {
-        background: none;
-        border: none;
-        cursor: pointer;
-        padding: 0;
-    }
-    button ~ button {
-        margin-left: 5px;
-    }
-`;
-
-const Box = styled.div`
-    width: 50px;
-    height: 50px;
-    border-radius: 100%;
-    background: yellow;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 14px;
-    font-family: none;
-`;
 
 export default Home;
