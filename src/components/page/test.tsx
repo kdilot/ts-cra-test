202023bd5ec4ef4804b8a1b2704780aedffb26a8
kdilot <kdilot@gmail.com>
2020-07-12 11:50:57 +0900

    alert test

--- src/components/page/test.tsx
+++ src/components/page/test.tsx
@@ -1,61 +1,13 @@
 import React from 'react';
-import { TextField, Button, withStyles } from '@material-ui/core';
-import { Container, Enter, Title } from './styles';
+import Alert from 'common/components/Alert';
 
-type GreetingsProps = {
-    name: string;
-    mark: string;
-};
-
-const ColorButton = withStyles(() => ({
-    root: {
-        color: 'white',
-        width: '100px',
-        backgroundColor: 'black',
-        '&:hover': {
-            backgroundColor: 'black',
-        },
-    },
-}))(Button);
-const ColorText = withStyles(() => ({
-    root: {
-        width: '300px',
-        '& label.Mui-focused': {
-            color: 'black',
-        },
-        '& .MuiInput-underline:after': {
-            borderBottomColor: 'black',
-        },
-        '& .MuiOutlinedInput-root': {
-            '& fieldset': {
-                borderColor: 'black',
-            },
-            '&.Mui-focused fieldset': {
-                borderColor: 'black',
-            },
-        },
-    },
-}))(TextField);
-
-const Greetings: React.FC<GreetingsProps> = ({ name, mark }) => (
-    <Container>
-        <Title>TRIZ STUDIO</Title>
-        <Enter>
-            <ColorText
-                id="outlined-basic"
-                label="패스워드"
-                variant="outlined"
-                type="password"
-            />
-            <ColorButton variant="contained" style={{ marginLeft: '10px' }}>
-                접속
-            </ColorButton>
-        </Enter>
-    </Container>
+const Greetings: React.FC = () => (
+    <div>
+        asdfsad
+        <Alert />
+        <Alert />
+        <Alert />
+    </div>
 );
-
-Greetings.defaultProps = {
-    mark: 'tstetset',
-};
 
 export default Greetings;
