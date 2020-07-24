950d3ef2c65ed2236bb26e658b09fe6f3b687f2c
kdilot <kdilotdev@gmail.com>
2020-07-24 17:51:10 +0900

    [UPDATE] codeblock

--- src/components/page/Codeblock.tsx
+++ src/components/page/Codeblock.tsx
@@ -1,75 +1,84 @@
 import React from 'react';
-import { CopyBlock, dracula } from 'react-code-blocks';
-
-const TEST = `import React from 'react';
-import { CSVLink } from 'react-csv';
-
-const headers = [
-    { label: 'First Name', key: 'firstname' },
-    { label: 'Last Name', key: 'lastname' },
-    { label: 'Email', key: 'email' },
-];
-
-const data = [
-    { firstname: 'Ahmed', lastname: 'Tomi', email: '한글은요?' },
-    { firstname: 'Raed', lastname: '한글되나요', email: 'rl@smthing.co.com' },
-    { firstname: '한글도좀', lastname: 'Min l3b', email: 'ymin@cocococo.com' },
-];
-
-const CSV: React.FC = () => (
-    <div>
-        <CSVLink data={data} headers={headers}>
-            Download me
-        </CSVLink>
-        ;
-    </div>
-
-const CSV: React.FC = () => (
-    <div>
-        <CSVLink data={data} headers={headers}>
-            Download me
-        </CSVLink>
-        ;
-    </div>
-
-const CSV: React.FC = () => (
-    <div>
-        <CSVLink data={data} headers={headers}>
-            Download me
-        </CSVLink>
-        ;
-    </div>
-
-const CSV: React.FC = () => (
-    <div>
-        <CSVLink data={data} headers={headers}>
-            Download me
-        </CSVLink>
-        ;
-    </div>
-
-const CSV: React.FC = () => (
-    <div>
-        <CSVLink data={data} headers={headers}>
-            Download me
-        </CSVLink>
-        ;
-    </div>
-);
-
-export default CSV;`;
+import { CodeBlock, dracula } from 'react-code-blocks';
+import { code } from 'components/codeblock/code';
+import styled from 'styled-components';
 
 const Codeblock: React.FC = () => (
-    <div>
-        <CopyBlock
-            language={'jsx'}
-            text={TEST}
-            showLineNumbers={true}
-            theme={dracula}
-            wrapLines={true}
-            codeBlock
-        />
-    </div>
+    <Container>
+        <LeftLayout className="layout">
+            <Example>sdf</Example>
+            <Props>sdf</Props>
+        </LeftLayout>
+        <RightLayout className="layout">
+            <CodeBlock
+                language={'jsx'}
+                text={code}
+                showLineNumbers={true}
+                theme={dracula}
+                wrapLines={true}
+            />
+        </RightLayout>
+    </Container>
 );
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 1200px;
+    height: 600px;
+    margin: 0 auto;
+    padding: 1rem;
+    background: #cbae82;
+    flex-wrap: wrap;
+
+    .layout ~ .layout {
+        margin-left: 1rem;
+    }
+`;
+
+const Box = styled.div`
+    width: 100%;
+    height: 100%;
+    border: 2px solid #fff;
+    border-radius: 4px;
+`;
+
+const LeftLayout = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`;
+
+const RightLayout = styled(Box as any)`
+    display: flex;
+    flex: 1;
+    overflow: auto;
+    height: 100%;
+`;
+
+const Example = styled(Box as any)`
+    display: flex;
+    flex: 1;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    background: red;
+`;
+
+const Props = styled(Box as any)`
+    display: flex;
+    flex: 1;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    margin-top: 1rem;
+    background: purple;
+`;
 
 export default Codeblock;
