cf428366784182ac925b0e95cb939c4ea7633af9
kdilot <kdilotdev@gmail.com>
2020-07-27 17:57:43 +0900

    test

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import ImageUploader from 'modules/imageUpload/index';
+import Background from 'modules/imageUpload/svg/Background';
 
 const Home: React.FC = () => {
     return (
-        <ImageUploader
-            // buttonStyles={'TEST'}
-            buttonText={'TEST'}
-            withIcon={false}
-            withLabel={false}
-            withPreview={true}
-            maxFileSize={5242880 * 10}
-            imgExtension={['.jpg', '.jpeg', '.png', '.gif']}
-            onChange={(e) => console.log(e)}
-        />
+        <>
+            <ImageUploader
+                // buttonStyles={'TEST'}
+                buttonText={'TEST'}
+                withIcon={false}
+                withLabel={false}
+                withPreview={true}
+                maxFileSize={5242880 * 10}
+                imgExtension={['.jpg', '.jpeg', '.png', '.gif']}
+                onChange={(e) => console.log(e)}
+            />
+            <div style={{ width: '50px' }}>
+                <Background />
+            </div>
+        </>
     );
 };
 
