1473ff97f8545f2585e5c048bee0947abbe1570a
kdilot <kdilot@gmail.com>
2020-09-12 14:06:43 +0900

    test

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Home: React.FC = () => {
+    return (
+        <Container>
+            <Link to="/lottie">Lottie 이미지</Link>
+            <Link to="/editor/draftjs">Draftjs 에디터</Link>
+            <Link to="/codeblock">코드 Preview</Link>
+            <Link to="/board">게시판</Link>
+            <Link to="/csv">CSV 다운로드</Link>
+            <Link to="/image-uploader">이미지 업로더</Link>
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 500px;
+    justify-content: flex-start;
+    align-items: center;
+    a {
+        width: 250px;
+        padding: 5px 10px;
+    }
+`;
+
+export default Home;
---
+++ src/pages/imageUploader/index.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import ImageUploader from 'modules/imageUpload/index';
+import styled from 'styled-components';
+
+const ImageUpload: React.FC = () => {
+    const [images, setImages] = useState<string[]>([]);
+    return (
+        <Container>
+            <ImageUploader
+                withPreview={true}
+                maxFileSize={5}
+                imgExtension={['.jpg', '.jpeg', '.png']}
+                defaultImages={images}
+                limit={10}
+                onChange={(file: File[], base: any[]) => setImages(base)}
+            />
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 425px;
+    background: lightyellow;
+`;
+
+export default ImageUpload;
