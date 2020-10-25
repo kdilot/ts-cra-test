2bfadabc719fa85e3ce550c3c23effac5dc3b7c9
kdilot <kdilot@gmail.com>
2020-10-25 12:40:30 +0900

    search

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,19 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import keywordList from './keyword.json';
 import { Input } from 'antd';
 import styled from 'styled-components';
 
 const Search: React.FC = () => {
     const [keyword, setKeyword] = useState<any>('');
+    const [list, setList] = useState<any[]>([]);
+
+    const onLiveKeyword = (word: string) => {
+        const index = word.indexOf(keyword);
+        const result = `${word.slice(
+            0,
+            index,
+        )}<span>${keyword}</span>${word.slice(index + keyword.length)}`;
+        return result;
+    };
+
+    useEffect(() => {
+        if (!keyword.trim()) {
+            setList([]);
+            return;
+        }
+        const result = keywordList
+            .filter((data) => data.indexOf(keyword) !== -1)
+            .map((data) => onLiveKeyword(data));
+        setList(result);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [keyword]);
+
     return (
         <Container>
             <Input
                 value={keyword}
                 onChange={(e) => setKeyword(e.target.value)}
             />
-            <TextListLayout>
-                asdfasdf<span>fwew</span>sadf
-            </TextListLayout>
+            {list.map((word: string, index: number) => (
+                <TextListLayout
+                    key={index}
+                    dangerouslySetInnerHTML={{ __html: word }}
+                />
+            ))}
         </Container>
     );
 };
@@ -29,7 +55,6 @@ const Container = styled.section`
 
 const TextListLayout = styled.div`
     display: flex;
-    height: 50px;
     width: 100%;
     span {
         color: red;
