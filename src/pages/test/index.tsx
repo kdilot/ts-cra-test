842c57fb83b48f653ff1a489c2b1d7d1808ef151
kdilot <kdilot@gmail.com>
2020-12-10 11:29:33 +0900

    test

---
+++ src/pages/test/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+    vertical?: boolean;
+    size?: number;
+}
+
+const Divider: React.FC<Props> = ({
+    children,
+    vertical = false,
+    size = 5,
+}: any) => {
+    console.log(children);
+    // return <Container>{children}</Container>;
+    return (
+        <Container className={vertical && 'vertical'}>
+            {children.length > 0
+                ? children.map((list: any, index: number) => (
+                      <ChildrenLayout
+                          key={index}
+                          className={vertical && 'vertical'}
+                          size={size}>
+                          {list}
+                      </ChildrenLayout>
+                  ))
+                : children}
+        </Container>
+    );
+};
+
+const Test: React.FC = () => {
+    return (
+        <div style={{ width: '200px', background: 'yellow' }}>
+            <Divider size={10}>
+                <div>fist</div>
+                <div>fist</div>
+                <div>fist</div>
+                <div>fist</div>
+            </Divider>
+        </div>
+    );
+};
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+
+    &.vertical {
+        flex-direction: column;
+    }
+`;
+
+const ChildrenLayout = styled.div<any>`
+    & ~ & {
+        margin-left: ${(props: any) => `${props.size}px`};
+        &.vertical {
+            margin-top: ${(props: any) => `${props.size}px`};
+            margin-left: 0;
+        }
+    }
+    padding: 5px 20px;
+    background: rgba(0, 0, 0, 0.1);
+    margin: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export default Test;
